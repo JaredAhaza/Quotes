db017df2ddea55634654d3ea5d95393149bee6c8
JaredAhaza <jaredahazq@gmail.com>
2018-04-23 17:24:22 +0300

    updated page

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -18,3 +18,32 @@ export class QuotesComponent implements OnInit {
   }
 
 }
+
+
+import { Component, OnInit, Input } from '@angular/core';
+import { Quote } from '../quotes'
+@Component({
+ selector: 'app-quotes-form',
+ templateUrl: './quotes-form.component.html',
+ styleUrls: ['./quotes-form.component.css']
+})
+export class QuotesFormComponent implements OnInit {
+ @Input() myArr
+
+ upVote(id) {
+   this.myArr[id].upvote()
+ }
+ downVote(id) {
+   this.myArr[id].downvote()
+ }
+ delete(id) {
+   this.myArr.splice(this.myArr[id])
+ }
+ constructor() {
+   this.myArr = []
+ }
+
+ ngOnInit() {
+ }
+
+}
